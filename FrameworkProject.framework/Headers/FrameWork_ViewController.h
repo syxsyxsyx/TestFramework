ebca60e9a739f96158b0d86275de5e6a7d01e361
syx <964607751@qq.com>
2019-04-29 11:31:18 +0800

    1.0

---
+++ FrameworkProject.framework/Headers/FrameWork_ViewController.h
@@ -0,0 +1,17 @@
+//
+//  FrameWork_ViewController.h
+//  TestFramework
+//
+//  Created by 薪起程 on 2019/4/27.
+//  Copyright © 2019 xinqicheng. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface FrameWork_ViewController : UIViewController
+
+@end
+
+NS_ASSUME_NONNULL_END
